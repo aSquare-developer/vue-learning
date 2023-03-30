fc0bfa57db6b1c0a7d03c8c1e78ab4095e1fc77f
aSquare <artur.anissimov@gmail.com>
2023-03-30 16:11:06 +0300

    Component Props Props Class Object Binding

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,28 @@
 export default {
     template: `
-                    <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 text-black disabled:cursor-not-allowed" v-model:disabled="processing">
-                        <slot />
-                    </button>
-                `,
+        <button 
+             v-bind:class="{
+                'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+                'bg-blue-600 hover:bg-blue-700': type === 'primary',
+                'bg-green-600 hover:bg-green-700': type === 'secondary',
+                'bg-gray-600 hover:bg-gray-700': type === 'muted',
+                'is-loading': processing
+            }" 
+            v-model:disabled="processing"
+        >
+            <slot />
+        </button>
+    `,
 
-        data() {
-    return {
-        processing: true
-    };
-}
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false
+        }
+    }
+
 }
