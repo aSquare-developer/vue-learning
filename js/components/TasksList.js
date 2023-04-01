73b2e2bc53c0530083005e98ffddffd3327fe942
aSquare <artur.anissimov@gmail.com>
2023-04-01 22:51:05 +0300

    It's All So Easy Assignment Tagging Sets

--- js/components/TasksList.js
+++ js/components/TasksList.js
@@ -5,11 +5,27 @@ export default {
 
     template: `
         <section v-show="tasks.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
+            <h2 class="font-bold mb-2">
+                {{ title }}
+                <span>({{ tasks.length }})</span>
+            </h2>
     
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <div class="flex gap-2">
+                <button 
+                    v-on:click="currentTag = tag"
+                    v-for="tag in tags" 
+                    class="border rounded px-1 py-px text-xs"
+                    v-bind:class="{
+                        'border-blue-500 text-blue-500': tag === currentTag
+                    }"
+                >
+                    {{ tag }}
+                </button>
+            </div>
+            
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <task 
-                    v-for="task in tasks"
+                    v-for="task in filteredTasks"
                     :key="task.id"
                     v-bind:task="task"
                 ></task>
@@ -20,5 +36,25 @@ export default {
     props: {
         tasks: Array,
         title: String
+    },
+
+    data() {
+        return {
+            currentTag: 'All'
+        };
+    },
+
+    computed: {
+        filteredTasks() {
+            if(this.currentTag === 'All') {
+                return this.tasks;
+            }
+
+            return this.tasks.filter(a => a.tag === this.currentTag);
+        },
+
+        tags() {
+            return ['All', ...new Set(this.tasks.map(a => a.tag))];
+        }
     }
 }
